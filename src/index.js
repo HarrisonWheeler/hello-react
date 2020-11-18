4d6abe4faf1b0250e53fed88b9bfa51dcf68b3d6
Harrison Wheeler <wheelerharrison93@gmail.com>
2020-11-18 11:06:03 -0700

    hello-react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Hello(props) {
+  console.log(props);
+  return (
+    <div>
+      <h1>Welcome to {props.library}!</h1>
+      <p>{props.message}</p>
+      <p>{props.number} Props Total</p>
+    </div>
+  )
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Hello library="React" message="Have Fun!" number={3} />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// components always need to be capitalized!!
+
