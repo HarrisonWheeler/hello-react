eb3da990e315ff4e07853f665b68328dc688ff01
Harrison Wheeler <wheelerharrison93@gmail.com>
2020-11-19 11:12:28 -0700

    useEffect fetch to get data from github api

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 
+function GitHubUser({ login }) {
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then(res => res.json())
+      .then(setData)
+      .catch(console.error)
+  }, [])
+
+  if (data) {
+    return (
+      <div>
+        <h1>{data.login}</h1>
+        <img src={data.avatar_url} width={300} />
+      </div>
+    )
+  }
+  return null
+}
+
 function App() {
-  const [year, setYear] = useState(2050)
-  const [manager, setManager] = useState("Alex")
-  const [status, setStatus] = useState("Open")
-  return (
-    <>
-      <div>
-        <h1>{year}</h1>
-        <button onClick={() => setYear(year + 1)}>New Year!</button>
-      </div>
-      <div>
-        <h1>Manager on Duty: {manager}</h1>
-        <button onClick={() => setManager("Rachel")}>New Manager</button>
-      </div>
-      <div>
-        <h1>Status: {status}</h1>
-        <button onClick={() => setStatus("Open")}>Open</button>
-        <button onClick={() => setStatus("Back in 5")}>Break</button>
-        <button onClick={() => setStatus("Closed")}>Closed</button>
-      </div>
-    </>
-  )
+  return <GitHubUser login="harrisonwheeler" />
 }
 
 
