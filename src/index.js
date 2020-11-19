6dfa54c4fc02d72365e75faa3c0a537ed85f9d0c
Harrison Wheeler <wheelerharrison93@gmail.com>
2020-11-19 11:50:00 -0700

    react production build practce

--- src/index.js
+++ src/index.js
@@ -1,32 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 
-function GitHubUser({ login }) {
-  const [data, setData] = useState(null)
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${login}`)
-      .then(res => res.json())
-      .then(setData)
-      .catch(console.error)
-  }, [])
+function Checkbox() {
+  const [checked, toggle] = useReducer(checked => !checked, false)
 
-  if (data) {
-    return (
-      <div>
-        <h1>{data.login}</h1>
-        <img src={data.avatar_url} width={300} />
-      </div>
-    )
-  }
-  return null
+
+  return (
+    <>
+      <input type="checkbox" value={checked} onChange={toggle} />
+      {checked ? "checked" : "not checked"}
+    </>
+  )
 }
+
 
 function App() {
-  return <GitHubUser login="harrisonwheeler" />
+  return <Checkbox />
 }
-
 
 ReactDOM.render(
   <App />,
