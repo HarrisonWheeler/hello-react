b258a6fbb5d97481299ad5b90c6f84233c28aa67
Harrison Wheeler <wheelerharrison93@gmail.com>
2020-11-19 10:45:22 -0700

    useState/hooks practice

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 
-
-function Lake() {
-  return (
-    <h1>Lake!</h1>
-  )
-}
-
-function SkiResort() {
-  return (
-    <h1>Ski Resort!</h1>
-  )
-}
-
 function App() {
+  const [year, setYear] = useState(2050)
+  const [manager, setManager] = useState("Alex")
+  const [status, setStatus] = useState("Open")
   return (
     <>
-      <Lake />
-      <SkiResort />
+      <div>
+        <h1>{year}</h1>
+        <button onClick={() => setYear(year + 1)}>New Year!</button>
+      </div>
+      <div>
+        <h1>Manager on Duty: {manager}</h1>
+        <button onClick={() => setManager("Rachel")}>New Manager</button>
+      </div>
+      <div>
+        <h1>Status: {status}</h1>
+        <button onClick={() => setStatus("Open")}>Open</button>
+        <button onClick={() => setStatus("Back in 5")}>Break</button>
+        <button onClick={() => setStatus("Closed")}>Closed</button>
+      </div>
     </>
   )
 }
